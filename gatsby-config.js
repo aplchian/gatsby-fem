c400518e07fd97e16b90837f2e4fc72f15b7e203
alexander boquist <aplchian@alexanders-MacBook-Pro.local>
2019-09-29 18:12:25 -0400

    finished graphql site data

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Alex Boquist',
+    description: "Alex Boquist's site.",
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import Layout from '../components/layout';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello!</p>
+    </Layout>
+  </>
+);
