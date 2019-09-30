d452f172c87c6198e222bc6a7fc6f7a41856c924
alexander boquist <aplchian@alexanders-MacBook-Pro.local>
2019-09-30 18:01:41 -0400

    add mdx posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,23 @@ module.exports = {
     title: 'Alex Boquist',
     description: "Alex Boquist's site.",
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'src/posts'
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from '../components/layout';
-
-export default () => (
-  <>
+import usePosts from '../hooks/use-posts';
+import PostsPreview from '../components/post-preview';
+export default () => {
+  const posts = usePosts();
+  return (
     <Layout>
       <h1>Home</h1>
       <p>Hello!</p>
+      <h2>Read My Blog</h2>
+      {posts.map(post => {
+        return <PostsPreview key={post.slug} post={post} />;
+      })}
     </Layout>
-  </>
-);
+  );
+};
